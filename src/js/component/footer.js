1530adde45a553df734635bba24ff29f332378a3
Gerald2021 <506geraldaguilar@gmail.com>
2021-02-10 22:14:10 +0000

    primer commit

---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<footer className="footer mt-auto py-3 bg-dark text-light text-center">
+			<div>
+				<span>Place sticky footer content here.</span>
+			</div>
+		</footer>
+	);
+}
